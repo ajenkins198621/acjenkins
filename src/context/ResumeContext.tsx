5d62babdb53610e115611de27f1dad4cdba962c2
Austin Jenkins <austinclarkjenkins@gmail.com>
2023-07-26 17:21:56 -0600

    update resume url

--- src/context/ResumeContext.tsx
+++ src/context/ResumeContext.tsx
@@ -64,7 +64,7 @@ export const initialResume: IResume = {
         },
         {
             name: "Resume",
-            url: "https://resume.io/r/3wlNvJLMB",
+            url: "https://resume.io/r/uCJjsnoDX",
             icon: faFile
         }
 

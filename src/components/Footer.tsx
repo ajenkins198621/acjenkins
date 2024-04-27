0bdf9a6bd5bc96c137ad47d121591fa4500f0802
Austin Jenkins <austinclarkjenkins@gmail.com>
2024-04-27 14:34:54 -0600

    make footer static to fix SSR error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
             <div className="container">
                 <div className="footer-title">That&apos;s all folks!</div>
                 <div className="footer-description">I designed and built this site.<br />
-You can see the Figma and code at the Github Repo.</div>
-                <div className="copyright">&copy; Austin Jenkins {(new Date()).getFullYear()}</div>
+                    You can see the Figma and code at the Github Repo.</div>
+                <div className="copyright">&copy; Austin Jenkins 2024</div>
             </div>
         </footer>
     )
